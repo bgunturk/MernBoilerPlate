0d3d003222c5bad51ba2ffa1e212444d2ed3fb38
bgunturk <71447509+bgunturk@users.noreply.github.com>
2021-01-28 12:51:17 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const { User } = require('./models/user');
+const config = require('./config/key');
+
 mongoose
-.connect('mongodb+srv://bulentgunturk:1234512345Bg@cluster0.kshxj.mongodb.net/Cluster0?retryWrites=true&w=majority', {
+.connect(config.mongoURI, {
 useUnifiedTopology: true,
 useNewUrlParser: true,
 })
@@ -13,6 +18,27 @@ console.log(`${err.message}`);
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
-  })
+  });
 
-app.listen(5000);
+app.use(bodyParser.urlencoded({ extended:true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.post('/api/users/register' , function (req, res) {
+
+    const user= new User(req.body)
+    user.save((err, document) => {   
+        if(err) return res.json ({ success: false, err})
+    });
+    return res.status(200).json({
+        success: true,
+    });
+  });
+
+
+
+  const port = process.env.PORT || 5000
+
+  app.listen(port, () => {
+    console.log(`Server Listening on ${port}`)
+  });
